7738c87a77a69ad84828f0a029af99983787b393
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-11-23 21:25:19 +0100

    Add Add column func

--- source/components/spreadsheet.jsx
+++ source/components/spreadsheet.jsx
@@ -1,4 +1,5 @@
 var React = require("react");
+var _ = require("underscore");
 var Row = require("./row.jsx").Row;
 var update = require("react-addons-update");
 
@@ -10,9 +11,37 @@ module.exports.Spreadsheet = React.createClass({
   },
 
   addRow: function() {
-    var newState = update(this.state, {
-      rows: {$push: [[""]]}
+    var numCols,
+        newRow,
+        newState;
+
+    numCols = this.state.rows[0].length;
+    newRow = _.range(numCols).map(function(){
+      return "";
     });
+
+    newState = update(this.state, {
+      rows: {$push: [newRow]}
+    });
+
+    this.setState(newState);
+  },
+
+  addColumn: function() {
+    var newState = update(this.state, {
+      rows: {
+        $apply: function(rows) {
+          var _rows = rows.map(function(row){
+            var updatedRow = update(row, {
+              $push: [""]
+            });
+            return updatedRow;
+          });
+          return _rows;
+        }
+      }
+    });
+
     this.setState(newState);
   },
 
@@ -27,6 +56,7 @@ module.exports.Spreadsheet = React.createClass({
           <div className="container-fluid">
             <div className="navbar-header">
               <button className="btn btn-default navbar-btn" onClick={this.addRow}>Add row</button>
+              <button className="btn btn-default navbar-btn" onClick={this.addColumn}>Add column</button>
             </div>
           </div>
         </nav>
