11b2cbffa296da30c5e3c7a2cf6d88a6245f1912
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-11-23 20:30:20 +0100

    Add static state

--- source/components/excel.jsx
+++ source/components/excel.jsx
@@ -2,7 +2,17 @@ var React = require("react");
 var Row = require("./row.jsx").Row;
 
 module.exports.Excel = React.createClass({
+  getInitialState: function() {
+    return {
+      rows: [[""]]
+    };
+  },
+
   render: function() {
+    var rows = this.state.rows.map(function(row, i){
+      return <Row key={"row_" + i} cells={row} />;
+    });
+
     return (
       <div>
         <nav className="navbar navbar-default navbar-fixed-bottom" role="navigation">
@@ -13,7 +23,7 @@ module.exports.Excel = React.createClass({
         </nav>
         <table className="table table-bordered">
           <tbody>
-            <Row />
+            { rows }
           </tbody>
         </table>
       </div>
--- source/components/row.jsx
+++ source/components/row.jsx
@@ -3,9 +3,13 @@ var Cell  = require("./cell.jsx").Cell;
 
 module.exports.Row = React.createClass({
   render: function() {
+    var cells = this.props.cells.map(function(cell, i){
+      return <Cell key={"cell_" + i} />;
+    });
+
     return (
       <tr>
-        <Cell />
+        { cells }
       </tr>
     );
   }
