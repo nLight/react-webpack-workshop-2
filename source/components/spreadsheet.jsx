6a49efb28d587ccea9b4851a36d2c25803ef54a6
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-11-23 20:52:46 +0100

    Add Add row button

--- source/components/spreadsheet.jsx
+++ source/components/spreadsheet.jsx
@@ -1,11 +1,19 @@
 var React = require("react");
 var Row = require("./row.jsx").Row;
+var update = require("react-addons-update");
 
 module.exports.Spreadsheet = React.createClass({
   getInitialState: function() {
     return {
       rows: [[""]]
     };
+  },
+
+  addRow: function() {
+    var newState = update(this.state, {
+      rows: {$push: [[""]]}
+    });
+    this.setState(newState);
   },
 
   render: function() {
@@ -18,6 +26,7 @@ module.exports.Spreadsheet = React.createClass({
         <nav className="navbar navbar-default navbar-fixed-bottom" role="navigation">
           <div className="container-fluid">
             <div className="navbar-header">
+              <button className="btn btn-default navbar-btn" onClick={this.addRow}>Add row</button>
             </div>
           </div>
         </nav>
