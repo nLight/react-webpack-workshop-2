3cce3407910c7e2fe6595e65245ed691fa876ce2
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-11-23 20:21:39 +0100

    Add row component

---
+++ source/components/row.jsx
@@ -0,0 +1,11 @@
+var React = require("react");
+
+module.exports.Row = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>Row</td>
+      </tr>
+    );
+  }
+});
--- source/components/spreadsheet.jsx
+++ source/components/spreadsheet.jsx
@@ -1,4 +1,5 @@
 var React = require("react");
+var Row = require("./row.jsx").Row;
 
 module.exports.Spreadsheet = React.createClass({
   render: function() {
@@ -12,6 +13,7 @@ module.exports.Spreadsheet = React.createClass({
         </nav>
         <table className="table table-bordered">
           <tbody>
+            <Row />
           </tbody>
         </table>
       </div>
