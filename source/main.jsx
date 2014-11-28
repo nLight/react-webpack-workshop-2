a5cb050553413b45e50f6c956acc052a4c78da54
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-11-28 23:31:31 +0100

    Add main component

--- source/main.jsx
+++ source/main.jsx
@@ -2,3 +2,9 @@ require('bootstrap/dist/css/bootstrap.css');
 require('./styles/main.less');
 
 var React = require("react/lib/ReactWithAddons");
+var Excel = require("./components/excel.jsx").Excel;
+
+React.render(
+  <Excel />,
+  document.getElementById('content')
+);
